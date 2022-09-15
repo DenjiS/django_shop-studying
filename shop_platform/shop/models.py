a40489fc860fbba01211d45c84e0204b9a42de18
Daniil <sychkov.daniil@yandex.ru>
2022-09-15 16:38:53 +0300

    --

--- shop_platform/shop/models.py
+++ shop_platform/shop/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 def get_path(instance, filename):
-    # file will be uploaded to /media/images/type/id/<filename>
+    # file will be uploaded to /media/images/<filename>
     return f'images/{instance.relation}/{filename}'
 
 
